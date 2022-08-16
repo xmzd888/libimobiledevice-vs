2c0a7789ff4d9e10e8cb1230d8963c001c111d5d
Caesar <453742103@qq.com>
2022-08-16 15:25:00 +0800

    no message

--- get-source.cmd
+++ get-source.cmd
@@ -1,6 +1,6 @@
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libplist
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libusbmuxd
-git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libimobiledevice
+git clone --single-branch --branch msvc/master https://github.com/xmzd888/libimobiledevice
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ideviceinstaller
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libirecovery
